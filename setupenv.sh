29ea9447ce85ff5defaa13449874d10ad9bcaae1
Timoshenko <softctrl@users.noreply.github.com>
2016-11-16 09:28:54 -0200

    Update setupenv.sh

--- setupenv.sh
+++ setupenv.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
-sudo curl https://raw.githubusercontent.com/softctrl/linux-scripts/master/b64 > /bin/b64
-sudo curl https://raw.githubusercontent.com/softctrl/linux-scripts/master/backup > /bin/backup
-sudo curl https://raw.githubusercontent.com/softctrl/linux-scripts/master/diffpaths > /bin/diffpaths
-sudo curl https://raw.githubusercontent.com/softctrl/linux-scripts/master/utf8convert > /bin/utf8convert
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+echo "https://raw.githubusercontent.com/softctrl/linux-scripts/master/b64";
+curl https://raw.githubusercontent.com/softctrl/linux-scripts/master/b64 > /bin/b64;
+echo "https://raw.githubusercontent.com/softctrl/linux-scripts/master/backup";
+curl https://raw.githubusercontent.com/softctrl/linux-scripts/master/backup > /bin/backup;
+echo "https://raw.githubusercontent.com/softctrl/linux-scripts/master/diffpaths";
+curl https://raw.githubusercontent.com/softctrl/linux-scripts/master/diffpaths > /bin/diffpaths;
+echo "https://raw.githubusercontent.com/softctrl/linux-scripts/master/utf8convert";
+curl https://raw.githubusercontent.com/softctrl/linux-scripts/master/utf8convert > /bin/utf8convert;
 
-sudo chmod +x /bin/b64
-sudo chmod +x /bin/backup
-sudo chmod +x /bin/diffpaths
-sudo chmod +x /bin/utf8convert
+chmod +x /bin/b64;
+chmod +x /bin/backup;
+chmod +x /bin/diffpaths;
+chmod +x /bin/utf8convert;
 
-echo "Done!!"
+echo "Done!!";
